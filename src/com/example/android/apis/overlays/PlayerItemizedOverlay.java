86af22565475a39dc777fa1a23fe79de5c6aab1d
Ben Lickly <blickly@eecs>
2011-02-22 08:59:59 -0800

    Removed unused imports

--- src/com/example/android/apis/overlays/PlayerItemizedOverlay.java
+++ src/com/example/android/apis/overlays/PlayerItemizedOverlay.java
@@ -1,13 +1,8 @@
 package com.example.android.apis.overlays;
 
-import java.util.ArrayList;
-import java.util.Random;
-
 import android.graphics.drawable.Drawable;
 
 import com.example.android.apis.model.Player;
-import com.example.android.apis.model.Robot;
-import com.google.android.maps.GeoPoint;
 import com.google.android.maps.ItemizedOverlay;
 import com.google.android.maps.OverlayItem;
 
