6440c6d725070571d24b0bb1d6fa326bb311614a
Ben Lickly <blickly@eecs>
2011-02-26 14:51:28 -0800

    Corrected outdated comments

--- src/com/ghostrun/activity/MapViewDemo.java
+++ src/com/ghostrun/activity/MapViewDemo.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2007 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.ghostrun.activity;
 
 import java.util.List;
@@ -34,26 +18,7 @@ import com.google.android.maps.MapView;
 import com.google.android.maps.MyLocationOverlay;
 import com.google.android.maps.Overlay;
 
-/**
- * Example of how to use an {@link com.google.android.maps.MapView}.
- * <h3>MapViewDemo</h3>
-
-<p>This demonstrates creating a Map based Activity.</p>
-
-<h4>Demo</h4>
-Views/MapView
-
-<h4>Source files</h4>
- * <table class="LinkTable">
- *         <tr>
- *             <td >src/com.ghostrun/view/MapViewDemo.java</td>
- *             <td >The Alert Dialog Samples implementation</td>
- *         </tr>
- *         <tr>
- *             <td >/res/layout/mapview.xml</td>
- *             <td >Defines contents of the screen</td>
- *         </tr>
- * </table>
+/** Map Activity for showing the status of a game in progress.
  */
 public class MapViewDemo extends MapActivity {
     MapView mapView;
