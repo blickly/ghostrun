8ddc776479c2f4e10f9afd4e7c3f39d03ba3ab60
Ben Lickly <blickly@eecs>
2011-03-03 14:30:18 -0800

    Added Robot ability to move toward goal.
    This replaces the hack from before of moving
    halfway to a goal point (the player), replacing it
    with code for the Robot to move at it's own speed.
    This means that robots can now chase the player at
    a constant speed!

--- src/com/ghostrun/model/Robot.java
+++ src/com/ghostrun/model/Robot.java
@@ -5,6 +5,7 @@ import java.util.Random;
 import com.google.android.maps.GeoPoint;
 
 public class Robot {
+    final int ROBOT_SPEED = 200;
     private GeoPoint location;
     Player player;
     
@@ -19,6 +20,14 @@ public class Robot {
 
     public GeoPoint getLocation() {
         return location;
+    }
+    
+    public void updateLocation() {
+        if (player.hasLocation()) {
+            moveTowardPoint(player.getLocationAsGeoPoint());
+        } else {
+            moveRandomly(ROBOT_SPEED);
+        }
     }
 
     /** Move robot randomly from existing position by a distance that is
@@ -37,16 +46,48 @@ public class Robot {
         setLocation(new GeoPoint(lat, lon));
     }
 
-    public void moveTowardPlayer() {
-        // FIXME: Race condition between hasLocation and getLocationAsGeoPoint
-        if (!player.hasLocation()) return;
-        GeoPoint oldLocation = getLocation();
-        GeoPoint playerLocation = player.getLocationAsGeoPoint();
-        int lat = (oldLocation.getLatitudeE6()
-                + playerLocation.getLatitudeE6()) / 2;
-        int lon = (oldLocation.getLongitudeE6()
-                + playerLocation.getLongitudeE6()) / 2;
-        setLocation(new GeoPoint(lat, lon));
+    /** Move robot toward the given point by a distance up to ROBOT_SPEED
+     *  (or to the point if it is closer than ROBOT_SPEED.
+     *  @param point The desired destination point.
+     */
+    public void moveTowardPoint(GeoPoint point) {
+        if (point == null) {
+            return;
+        }
+        GeoPointOffset direction = new GeoPointOffset(location, point);
+        double totalDistance = direction.getLength();
+        if (ROBOT_SPEED > totalDistance) {
+            location = point;
+        } else {
+            direction.scaleBy(ROBOT_SPEED / totalDistance);
+            location = direction.addTo(location);
+        }
+    }
+    
+    private class GeoPointOffset {
+        private int deltaLat;
+        private int deltaLon;
+
+        public GeoPointOffset(GeoPoint start, GeoPoint end) {
+            this.deltaLat = end.getLatitudeE6() - start.getLatitudeE6();
+            this.deltaLon = end.getLongitudeE6() - start.getLongitudeE6();
+        }
+
+        public GeoPoint addTo(GeoPoint p) {
+            int lat = p.getLatitudeE6() + this.deltaLat;
+            int lon = p.getLongitudeE6() + this.deltaLon;
+            return new GeoPoint(lat, lon);
+        }
+
+        public void scaleBy(double factor) {
+            deltaLat = (int) (deltaLat * factor);
+            deltaLon = (int) (deltaLon * factor);
+        }
+
+        public double getLength() {
+            return Math.sqrt(deltaLat * deltaLat + deltaLon * deltaLon);
+        }
+        
     }
 
 }
