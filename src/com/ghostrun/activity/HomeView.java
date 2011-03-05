fbf059e33f114584f28d01e424655fef1534143a
tuanlecal <tuanlecal@yahoo.com>
2011-03-04 18:58:36 -0800

    homeview, xml file and buttons

---
+++ src/com/ghostrun/activity/HomeView.java
@@ -0,0 +1,30 @@
+package com.ghostrun.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.ghostrun.R;
+
+public class HomeView extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setContentView(R.layout.homeview);
+        Button play=(Button)findViewById(R.id.play);
+        
+        play.setOnClickListener(new View.OnClickListener() {
+        	@Override
+			public void onClick(View v) {
+        		// TODO Auto-generated method stub
+        		Intent i = new Intent(HomeView.this, MapViewDemo.class);
+        		startActivity(i);
+        	}
+        });
+    }
+}
