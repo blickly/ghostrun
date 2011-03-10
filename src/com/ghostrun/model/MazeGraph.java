3906de7db1ddd7667323da1f45e43ff34e5b7235
Ben Lickly <blickly@eecs>
2011-03-10 14:03:55 -0800

    Added functionality to MazeGraph.

--- src/com/ghostrun/model/MazeGraph.java
+++ src/com/ghostrun/model/MazeGraph.java
@@ -1,15 +1,28 @@
 package com.ghostrun.model;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 import com.google.android.maps.GeoPoint;
 
 public class MazeGraph {
 
     public MazeGraphPoint addPoint(GeoPoint point) {
-        return new MazeGraphPoint(point);
+        MazeGraphPoint mgPoint = new MazeGraphPoint(point);
+        points.add(mgPoint);
+        return mgPoint;
     }
     
     public void addEdge(MazeGraphPoint p1, MazeGraphPoint p2) {
-        
+        p1.addConncetionTo(p2);
+        p2.addConncetionTo(p1);
     }
+
+    public MazeGraphPoint getRandomPoint() {
+        Random rand = new Random();
+        return points.get(rand.nextInt(points.size()));
+    }
+
+    private ArrayList<MazeGraphPoint> points;
 
 }
--- src/com/ghostrun/model/MazeGraphPoint.java
+++ src/com/ghostrun/model/MazeGraphPoint.java
@@ -1,16 +1,23 @@
 package com.ghostrun.model;
 
+import java.util.HashSet;
+
 import com.google.android.maps.GeoPoint;
 
 public class MazeGraphPoint {
-    private GeoPoint location;
-
     public MazeGraphPoint(GeoPoint location) {
         this.location = location;
+        neighbors = new HashSet<MazeGraphPoint>();
     }
 
     public GeoPoint getLocation() {
         return location;
     }
 
+    public void addConncetionTo(MazeGraphPoint other) {
+        neighbors.add(other);
+    }
+
+    private GeoPoint location;
+    private HashSet<MazeGraphPoint> neighbors;
 }
