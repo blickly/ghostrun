986f571e16e9aea901bdf9214feaa21ddf98122d
TuanLe <tuanlecal@yahoo.com>
2011-03-15 18:15:59 -0700

    example of how to generate MazeGraph from maze.xml. input file is stored in assets folder.

--- src/com/ghostrun/activity/HomeView.java
+++ src/com/ghostrun/activity/HomeView.java
@@ -1,5 +1,7 @@
 package com.ghostrun.activity;
 
+import java.util.Vector;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
@@ -8,6 +10,9 @@ import android.view.View;
 import android.widget.Button;
 
 import com.ghostrun.R;
+import com.ghostrun.model.Edge;
+import com.ghostrun.model.MazeGraph;
+import com.ghostrun.model.Parser;
 
 public class HomeView extends Activity {
     /** Called when the activity is first created. */
@@ -19,11 +24,34 @@ public class HomeView extends Activity {
         Button play=(Button)findViewById(R.id.play);
 
         play.setOnClickListener(new View.OnClickListener() {
-            @Override
+            //@Override
             public void onClick(View v) {
                 Intent i = new Intent(HomeView.this, GameMapView.class);
                 startActivity(i);
             }
         });
+        
+        Parser p = new Parser();
+        MazeGraph g=new MazeGraph();
+        try {
+            g=p.parse(getAssets().open("maze.xml"));
+            /*Vector<Vertex> v= (Vector<Vertex>)g.vertices();
+            for (Vertex vertex:v) {
+                System.out.println(vertex);
+            }*/
+            
+            /*Vector<Edge> e = (Vector<Edge>)g.edges();
+            for (Edge edge:e) {
+                System.out.println(edge);
+            }*/
+            
+        } catch (Exception e){
+            System.out.println("Exception found: "+e.getMessage());
+        }
+        
+        
+        
+        
     }
+    
 }
