8267c4dc8d20db8fa3d2406b96fbfdde909338a5
Ben Lickly <blickly@eecs>
2011-04-07 13:21:03 -0700

    Rewrote parser to follow XML structure.
    Previously it made assumptions about the ordering of all tags, which I
    think is a bit dangerous if we will be extending our XML schema in the
    future.

--- src/com/ghostrun/controllers/Parser.java
+++ src/com/ghostrun/controllers/Parser.java
@@ -2,13 +2,11 @@ package com.ghostrun.controllers;
 
 import java.io.InputStream;
 import java.util.HashMap;
-import java.util.Vector;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.w3c.dom.Document;
-import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
@@ -17,76 +15,70 @@ import com.ghostrun.model.MazeGraphPoint;
 import com.google.android.maps.GeoPoint;
 
 public class Parser {
-    private Vector<Integer> ids;
-    private Vector<Double> lats;
-    private Vector<Double> lons;
-    private Vector<Integer> src;
-    private Vector<Integer> dest;
-    private HashMap<Integer,MazeGraphPoint> hash_map;
-    
-    public Parser() {
-        ids = new Vector<Integer>();
-        lats = new Vector<Double>();
-        lons = new Vector<Double>();
-        src = new Vector<Integer>();
-        dest = new Vector<Integer>();
-        hash_map = new HashMap<Integer,MazeGraphPoint>();
+    private MazeGraph graph;
+    private HashMap<Integer, MazeGraphPoint> points;
+
+    private void initialize() {
+        graph = new MazeGraph();
+        points = new HashMap<Integer, MazeGraphPoint>();
     }
-    
+
     /**
-     * Construct a MazeGraph object from an input xml file.
-     * @param fileName
+     *  Construct a MazeGraph object from an input xml file.
+     *  @param instream Input stream to read from
+     *  @return The completed MazeGraph
      */
     public MazeGraph parse(InputStream instream) {
-        MazeGraph g = new MazeGraph();
+        initialize();
         try {
             DocumentBuilder doc_builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
             Document doc = doc_builder.parse(instream);
-            parsingATag(doc,"Node");
-            parsingATag(doc,"LatLng");
-            parsingATag(doc,"Edge");
-            
-            for (int i=0;i<ids.size();++i) {
-                MazeGraphPoint point = g.addPoint(new GeoPoint((int)(lats.get(i).doubleValue()*1E6),
-                                                               (int)(lons.get(i).doubleValue()*1E6)));
-                hash_map.put(ids.get(i), point);
+
+            NodeList nodes = doc.getElementsByTagName("Node");
+            if (nodes != null) {
+                for (int i=0; i < nodes.getLength(); ++i) {
+                    parseNode(nodes.item(i));
+                }
             }
-            
-            for (int i=0;i<src.size();++i) {
-                MazeGraphPoint srcVertex = hash_map.get(src.get(i));
-                MazeGraphPoint dstVertex = hash_map.get(dest.get(i));
-                g.addEdge(srcVertex,dstVertex);
+
+            NodeList edges = doc.getElementsByTagName("Edge");
+            if (edges != null) {
+                for (int i=0; i < edges.getLength(); ++i) {
+                    parseEdge(edges.item(i));
+                }
             }
-            
         } catch (Exception e) {
             System.out.print("Exception found in Parser.java: "+e.getMessage());
         }
-        return g;
+        return graph;
     }
-    
-    private void parsingATag(Document doc, String tagName) {
-        NodeList node_list = doc.getElementsByTagName(tagName);
-        if (node_list!=null) {
-            for (int i=0;i<node_list.getLength();++i) {
-                Node tag_node = node_list.item(i);
-                //System.out.println(tag_node.getNodeName());
-                NamedNodeMap node_attributes = tag_node.getAttributes();
-                for (int j=0;j<node_attributes.getLength();++j) {
-                    Node attribute = node_attributes.item(j);
-                    if (attribute.getNodeName().equals("id")) {
-                        ids.add(Integer.parseInt(attribute.getNodeValue()));
-                    } else if (attribute.getNodeName().equals("lat")) {
-                        lats.add(Double.parseDouble(attribute.getNodeValue()));
-                    } else if (attribute.getNodeName().equals("lon")) {
-                        lons.add(Double.parseDouble(attribute.getNodeValue()));
-                    } else if (attribute.getNodeName().equals("source")) {
-                        src.add(Integer.parseInt(attribute.getNodeValue()));
-                    } else if (attribute.getNodeName().equals("dest")) {
-                        dest.add(Integer.parseInt(attribute.getNodeValue()));
-                    }
-                    //System.out.println(attribute.getNodeName()+" "+attribute.getNodeValue());
-                }
-            }
-        }
+
+    private void addNode(int id, GeoPoint location) {
+        points.put(id, graph.addPoint(location));
+    }
+
+    private void addEdge(int node1, int node2) {
+        graph.addEdge(points.get(node1), points.get(node2));
+    }
+
+    private void parseNode(Node node) {
+        Node id = node.getAttributes().getNamedItem("id");
+        Node latLng = node.getFirstChild();
+        addNode(Integer.parseInt(id.getNodeValue()), parseLatLng(latLng));
+    }
+
+    private GeoPoint parseLatLng(Node latLngNode) {
+        Node lat = latLngNode.getAttributes().getNamedItem("lat");
+        Node lng = latLngNode.getAttributes().getNamedItem("lon");
+        return new GeoPoint(
+                (int)(Double.parseDouble(lat.getNodeValue())*1E6),
+                (int)(Double.parseDouble(lng.getNodeValue())*1E6));
+    }
+
+    private void parseEdge(Node edge) {
+        Node src = edge.getAttributes().getNamedItem("source");
+        Node dst = edge.getAttributes().getNamedItem("dest");
+        addEdge(Integer.parseInt(src.getNodeValue()),
+                Integer.parseInt(dst.getNodeValue()));
     }
 }
