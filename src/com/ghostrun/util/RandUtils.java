d2385abb6f37e375e53a10c186a79eaa3d7bcbe8
Ben Lickly <blickly@eecs>
2011-05-02 12:47:47 -0700

    Added time seeding (commented out for now)

--- src/com/ghostrun/util/RandUtils.java
+++ src/com/ghostrun/util/RandUtils.java
@@ -1,9 +1,11 @@
 package com.ghostrun.util;
 
+//import java.util.Date;
 import java.util.Random;
 
 public class RandUtils {
-    private static Random rand = new Random();
+//    private static Random rand = new Random(new Date().getTime());
+    private static Random rand = new Random(42);
 
     public static int nextInt(int i) {
         return rand.nextInt(i);
