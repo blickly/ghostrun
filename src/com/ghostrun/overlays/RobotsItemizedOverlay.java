36f51702b191b052281492d6672e06b3f9c64521
Ben Lickly <blickly@eecs>
2011-05-02 12:24:27 -0700

    Check robot types when deciding colors.

--- src/com/ghostrun/overlays/RobotsItemizedOverlay.java
+++ src/com/ghostrun/overlays/RobotsItemizedOverlay.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import android.graphics.drawable.Drawable;
 
-import com.ghostrun.R;
 import com.ghostrun.model.Robot;
 import com.google.android.maps.ItemizedOverlay;
 import com.google.android.maps.OverlayItem;
@@ -24,15 +23,6 @@ public class RobotsItemizedOverlay extends ItemizedOverlay<OverlayItem> {
 
         populate();
     }
-
-//    @Override
-//    protected boolean onTap(int index) {
-//        android.util.Log.d("RobotsItemizedOverlay", "Item tapped. Id: " + index
-//                + " Total items: " + size());
-//        robots.get(index).moveRandomly(500);
-//        populate();
-//        return true;
-//    }
     
     public void refresh() {
         populate();
@@ -42,17 +32,17 @@ public class RobotsItemizedOverlay extends ItemizedOverlay<OverlayItem> {
     protected OverlayItem createItem(int i) {
         Robot r = robots.get(i);
         OverlayItem ghost = new OverlayItem(r.getLocation(), "", "");
-        switch (i) {
-            case 0:
+        switch (r.getRobotType()) {
+            case BLINKY:
                 ghost.setMarker(redMarker);
                 break;
-            case 1:
+            case INKY:
                 ghost.setMarker(blueMarker);
                 break;
-            case 2:
+            case PINKY:
                 ghost.setMarker(pinkMarker);
                 break;
-            case 3:
+            case CLYDE:
                 ghost.setMarker(orangeMarker);
                 break;
         }
