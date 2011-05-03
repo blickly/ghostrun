bd986be813263c01bfb20a41079d2b33e820743b
TuanLe <tuanlecal@yahoo.com>
2011-05-03 12:20:55 -0700

    Stuff for Guide Activity.

---
+++ src/com/ghostrun/activity/GuideView.java
@@ -0,0 +1,25 @@
+package com.ghostrun.activity;
+
+import com.ghostrun.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class GuideView extends Activity {
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.guideview);
+        
+        // Stop the current activity and return to the previous view.
+        Button logobutton=(Button)findViewById(R.id.guideview_paclogo);
+        logobutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
