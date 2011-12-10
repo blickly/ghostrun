436c98fa5a9ec2b0264085d8606f5a7fd3166e36
Ben Lickly <blickly@eecs>
2011-12-09 16:39:49 -0800

    Improve logs for fake phone simulator

--- fake_phone_simulator/test.go
+++ fake_phone_simulator/test.go
@@ -2,29 +2,33 @@ package main
 
 import (
   "fmt"
-  "net"
   "http"
-  "time"
+  "log"
+  "net"
   "rand"
+  "time"
 )
 
-const DEBUG = true;
+//const SERVER = "localhost:8080";
+const SERVER = "pacmanplusplus.appspot.com:80";
+const LOGINFO = true;
 
 func main() {
-  num_phones := 5
-  run_time := 60 * int64(1000000000)   // in ns
-  update_rate := 5 * int64(1000000000) // in ns between requests
+  num_phones := 256
+  run_time := 90 * int64(1000000000)   // in ns
+  update_rate := int64(1000000000)/2   // in ns between requests
 
-  simulator_offset := 100000
-  for i := simulator_offset; i < simulator_offset+num_phones; i++ {
+  pid_offset := 100000
+  for i := pid_offset; i < pid_offset+num_phones; i++ {
     go run_phone(i, update_rate)
   }
   time.Sleep(run_time)
-  fmt.Println("Quitting...")
+  log.Println("Quitting...")
 }
 
 func run_phone(pid int, update_rate int64) {
   gid := 1
+  time.Sleep(rand.Int63n(update_rate))
   for ;; {
     lat := rand.Float32() * 90
     lng := rand.Float32() * 90
@@ -36,21 +40,24 @@ func run_phone(pid int, update_rate int64) {
 func post_position(gid int, pid int, lat float32, lng float32) {
   url := fmt.Sprintf("/post_position?gid=%d&pid=%d&lat=%f&lng=%f",
                      gid, pid, lat, lng)
-  con, err := net.Dial("tcp", "localhost:8080")
+  con, err := net.Dial("tcp", SERVER)
   if err != nil {
-    fmt.Println(err)
+    log.Printf("[ERR] from phone %d: %s", pid, err)
+    return
   }
   ccon := http.NewClientConn(con, nil)
   defer ccon.Close()
-  if DEBUG {
-    fmt.Println("Will post to ", url)
+  if LOGINFO {
+    log.Printf("[INFO] Will post to '%s'", url)
   }
   req, err := http.NewRequest("GET", url, nil)
   if err != nil {
-    fmt.Println(err)
+    log.Printf("[ERR] from phone %d: %s", pid, err)
+    return
   }
   err = ccon.Write(req)
   if err != nil {
-    fmt.Println(err)
+    log.Printf("[ERR] from phone %d: %s", pid, err)
+    return
   }
 }
