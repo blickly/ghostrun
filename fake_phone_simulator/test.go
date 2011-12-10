821a7583ccbf34e3f0ab3b3370947c749764d571
Ben Lickly <blickly@eecs>
2011-12-10 00:10:34 -0800

    Add logging of response from server and round trip time

--- fake_phone_simulator/test.go
+++ fake_phone_simulator/test.go
@@ -3,8 +3,9 @@ package main
 import (
   "fmt"
   "http"
+  "json"
   "log"
-  "net"
+  //"net"
   "rand"
   "time"
 )
@@ -14,9 +15,9 @@ const SERVER = "pacmanplusplus.appspot.com:80";
 const LOGINFO = true;
 
 func main() {
-  num_phones := 256
-  run_time := 90 * int64(1000000000)   // in ns
-  update_rate := int64(1000000000)/2   // in ns between requests
+  num_phones := 3
+  run_time := 20 * int64(1000000000)   // in ns
+  update_rate := 10*int64(1000000000)/2   // in ns between requests
 
   pid_offset := 100000
   for i := pid_offset; i < pid_offset+num_phones; i++ {
@@ -40,24 +41,15 @@ func run_phone(pid int, update_rate int64) {
 func post_position(gid int, pid int, lat float32, lng float32) {
   url := fmt.Sprintf("/post_position?gid=%d&pid=%d&lat=%f&lng=%f",
                      gid, pid, lat, lng)
-  con, err := net.Dial("tcp", SERVER)
+  requestTime := time.Nanoseconds()
+  resp, _, err := http.Get("http://" + SERVER + url)
+  responseTime := time.Nanoseconds()
   if err != nil {
     log.Printf("[ERR] from phone %d: %s", pid, err)
     return
   }
-  ccon := http.NewClientConn(con, nil)
-  defer ccon.Close()
-  if LOGINFO {
-    log.Printf("[INFO] Will post to '%s'", url)
-  }
-  req, err := http.NewRequest("GET", url, nil)
-  if err != nil {
-    log.Printf("[ERR] from phone %d: %s", pid, err)
-    return
-  }
-  err = ccon.Write(req)
-  if err != nil {
-    log.Printf("[ERR] from phone %d: %s", pid, err)
-    return
-  }
+  v := make(map[string]interface{},9)
+  json.NewDecoder(resp.Body).Decode(&v)
+  log.Printf("Request '%s' at local time %v, Response %v at time %v",
+              url, requestTime, v["locations"], responseTime)
 }
