4536e49c4e37feb527c25c2ba75ef89e0e023a08
Ben Lickly <blickly@eecs>
2011-12-13 09:52:40 -0800

    New log measuring script

--- appengine_server/measure.sh
+++ appengine_server/measure.sh
@@ -3,11 +3,11 @@
 
 LOGNAME="$1"
 
-grep "LatencyStats" $LOGNAME |
-awk 'BEGIN { sum=0; total=0}
-           { print "Sent:",$3,"Returned:",$NF,"Latency:",$NF-$3;
-             sum += $NF - $3; total++
-           }
-     END   { print "  Received:",total,
-                  "\n  Average latency:",sum/total,"ms"}';
+grep "LatencyStats" $LOGNAME | ./measure.py;
+#awk 'BEGIN { sum=0; total=0}
+#           { print "Sent:",$3,"Returned:",$NF,"Latency:",$NF-$3;
+#             sum += $NF - $3; total++
+#           }
+#     END   { print "  Received:",total,
+#                  "\n  Average latency:",sum/total,"ms"}';
 
